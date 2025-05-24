4ce15c75e0587580526255510934529769e00152
Davis Doe Mwenda <127217458+ShuaibDoe@users.noreply.github.com>
2025-05-24 03:57:38 +0300

    Add files via upload

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,13 @@
+#user input for financial details
+monthly_income = int(input("Enter your monthly income:"))
+monthly_expense = int(input("Enter your total monthly expense:"))
+
+#calculate monthly savings
+monthly_savings = monthly_income - monthly_expense
+
+#project annual savings
+annual_rate = 0.05
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print(monthly_savings)
+print(projected_savings)
