6f3d01d8c12d9d6b2265f41753b686f053909c2d
Davis Doe Mwenda <127217458+ShuaibDoe@users.noreply.github.com>
2025-06-07 02:05:00 +0300

    Add files via upload

---
+++ arithmetic_operations.py
@@ -0,0 +1,17 @@
+def perform_operation():
+    operation = 'add', 'subtract', 'multiply', 'divide'
+
+    def add(num1, num2):
+        return num1 + num2
+    
+    def subtract(num1, num2):
+        return num1 - num2
+    
+    def multiply(num1, num2):
+        return num1 * num2
+    
+    def divide(num1, num2):
+        if num2 == 0:
+            return "Error: Division by zero is not allowed"
+        return num1 / num2
+    
