5c587612447a0c6ca9e43aa7f647e6d5d153ad3e
Davis Doe Mwenda <127217458+ShuaibDoe@users.noreply.github.com>
2025-06-15 08:19:02 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,9 +8,10 @@ class BankAccount:
             
     def withdraw(self, amount):
         if amount <= self.account_balance:
-        self.account_balance -= amount
-        return True
-        return False
+            self.account_balance -= amount
+            return True
+        else:
+            return False
             
     def display_balance(self):
         return f"Current Balance: ${self.account_balance:.2f}"
