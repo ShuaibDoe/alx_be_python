7520534d5b66343f9378f7dfadcdc4637883eaf1
Davis Doe Mwenda <127217458+ShuaibDoe@users.noreply.github.com>
2025-06-15 07:59:20 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,20 +3,13 @@ class BankAccount:
         self.account_balance = account_balance
 
         def deposit(self, amount):
-            if amount > 0:
-                self.account_balance += amount
-                return f"Deposit: ${amount:.2f}"
-            else:
-                return "Deposit must be positive."
+            self.account_balance += amount
             
         def withdraw(self, amount):
-            if amount > 0 and amount <= self.account_balance:
-                self.account_balance -= amount
-                return f"Withdrawal: ${amount:.2f}"
-            elif amount > self.account_balance:
-                return "Insufficient funds."
-            else:
-                return "Withdrawal must be positive."
+            if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
             
         def display_balance(self):
             return f"Current Balance: ${self.account_balance:.2f}"
