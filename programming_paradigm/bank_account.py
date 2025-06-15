a3c16edb10a0c43fa049f7cf69e343f42ee68dcd
Davis Doe Mwenda <127217458+ShuaibDoe@users.noreply.github.com>
2025-06-15 08:21:32 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,20 +1,19 @@
 class BankAccount:
-    def __init__(self, account_balance=0):
-        self.account_balance = account_balance
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance  
 
     def deposit(self, amount):
-        if amount > 0:
-        self.account_balance += amount
-            
+        self.account_balance += amount  
+
     def withdraw(self, amount):
         if amount <= self.account_balance:
             self.account_balance -= amount
             return True
         else:
             return False
-            
+
     def display_balance(self):
-        return f"Current Balance: ${self.account_balance:.2f}"
+        print(f"Current balance: ${self.account_balance:.2f}")
             
 
         
